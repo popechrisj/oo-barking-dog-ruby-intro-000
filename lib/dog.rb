6af45c83222cff5fdcc86ccdb2b26f62a9167530
popechrisj@gmail.com <popechrisj@gmail.com>
2018-05-24 14:42:27 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Dog
+  def initialize(name, breed = "Mutt")
+    @name = name
+    @breed = breed
+  end
+  def name()
+    @name
+  end
+  def name=(name)
+    @name = name
+  end 
+  def bark()
+    puts "Woof!"
+  end
+end
